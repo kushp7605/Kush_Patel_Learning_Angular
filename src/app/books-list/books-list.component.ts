caedab17f38f8f9d9e8141ef430eb013ccbcb09a
Kush Patel <w0859776@myscc.ca>
2024-09-25 19:59:54 -0400

    Defined an array of 4 book items

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from '../Shared/Modules/book';
 
 @Component({
   selector: 'app-books-list',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './books-list.component.css'
 })
 export class BooksListComponent {
-
+  books: Book[] = [
+    { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: 1960, genre: 'Southern Gothic', },
+    { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', publicationYear: 1999, genre: 'Fantasy', rating: 4.7 },
+    { id: 3, title: 'Romeo and Juliet', author: 'William Shakespeare', publicationYear: 1597, genre: 'Tragedy', },
+    { id: 4, title: 'Marvel Comics', author: 'Stan Lee', publicationYear: 1939, genre: 'Superhero, Fantasy, Action, Adventure', rating: 5.0 },
+  ];
 }
