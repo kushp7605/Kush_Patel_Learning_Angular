32d0b8e38d22600d97bed050d36092f9e92dc7bb
Kush Patel <w0859776@myscc.ca>
2024-09-25 19:51:10 -0400

    Generated books-list-item component

---
+++ src/app/books-list-item/books-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-books-list-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './books-list-item.component.html',
+  styleUrl: './books-list-item.component.css'
+})
+export class BooksListItemComponent {
+
+}
