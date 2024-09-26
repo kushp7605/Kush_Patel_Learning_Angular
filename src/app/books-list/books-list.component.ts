a90e4c628d694342ec38e0f135fbac68a69dcbe4
Kush Patel <w0859776@myscc.ca>
2024-09-25 21:34:54 -0400

    Defined an array of 4 book items and imported CommonModule and BooksListItemComponent

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Book } from '../Shared/Modules/book';
 import { BooksListItemComponent } from '../books-list-item/books-list-item.component';
 
 @Component({
   selector: 'app-books-list',
   standalone: true,
-  imports: [BooksListItemComponent],
+  imports: [CommonModule, BooksListItemComponent],
   templateUrl: './books-list.component.html',
   styleUrl: './books-list.component.css'
 })
