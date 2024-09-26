7c9e895cef169518ecd403ecab2a9b189feae619
Kush Patel <w0859776@myscc.ca>
2024-09-25 20:55:25 -0400

    Implement BooksListItem component with book input and toggle ratings functionality

--- src/app/books-list-item/books-list-item.component.ts
+++ src/app/books-list-item/books-list-item.component.ts
@@ -1,14 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { Book } from '../Shared/Modules/book';
 
 @Component({
   selector: 'app-books-list-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './books-list-item.component.html',
   styleUrl: './books-list-item.component.css'
 })
 
 export class BooksListItemComponent {
-  @Input() book!: Book; // Ensure book is defined
+  @Input() book!: Book; // Accepts a Book object
+  @Input() showRating?: boolean; // Accepts showRating for visibility of ratings available for books or not
+
+  // Method to toggle the showRating property
+  toggleRating(): void {
+    this.showRating = !this.showRating;
+  }
 }
