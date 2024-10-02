cae09229893b5cd0c901c7aea4daee47823c8ab3
Kush Patel <w0859776@myscc.ca>
2024-10-02 15:08:15 -0400

    Created Lifecycle hook called ngOnInit() and added BooksListComponent to fetch and display books from BooksService

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Book } from '../Shared/Modules/book';
 import { BooksListItemComponent } from '../books-list-item/books-list-item.component';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-books-list',
   standalone: true,
   imports: [CommonModule, BooksListItemComponent],
   templateUrl: './books-list.component.html',
-  styleUrl: './books-list.component.css'
+  styleUrls: ['./books-list.component.css'],
 })
 
-export class BooksListComponent {
-  
+// Define the BooksListComponent class, implementing OnInit lifecycle hook
+export class BooksListComponent implements OnInit {
+  // Initialize an empty array to hold Book objects
+  books: Book[] = [];
+
+  constructor(private booksService: BooksService) {} // Dependency injection using constructor method
+
+  // Lifecycle hook that is called after the component is initialized
+  ngOnInit(): void {
+    // Call the getBooks method from BooksService and subscribe to the Observable
+    this.booksService.getBooks().subscribe((data: Book[]) => {
+      
+      // Assign the retrieved book data to the books property
+      this.books = data; 
+    });
+  }
 }
