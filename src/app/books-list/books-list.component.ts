5903ded55d8d79b83aaa3bfb87dd7de4d8c29ab2
Kush Patel <w0859776@myscc.ca>
2024-10-02 14:02:05 -0400

    Removed the books array values and move it to the mock-books.ts

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -12,10 +12,5 @@ import { BooksListItemComponent } from '../books-list-item/books-list-item.compo
 })
 
 export class BooksListComponent {
-  books: Book[] = [
-    { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: 1960, genre: 'Southern Gothic', },
-    { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', publicationYear: 1999, genre: 'Fantasy', rating: 4.7 },
-    { id: 3, title: 'Romeo and Juliet', author: 'William Shakespeare', publicationYear: 1597, genre: 'Tragedy', },
-    { id: 4, title: 'Marvel Comics', author: 'Stan Lee', publicationYear: 1939, genre: 'Superhero, Fantasy, Action, Adventure', rating: 5.0 },
-  ];
+  
 }
