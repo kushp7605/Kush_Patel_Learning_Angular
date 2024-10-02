f936b88d4f58dfe95b927c3fd4db8c5e2fe5a17a
Kush Patel <w0859776@myscc.ca>
2024-10-02 14:28:46 -0400

    Added a method to return an Observable of the BOOKS array

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Book } from '../Shared/Modules/book';
+import { BOOKS } from '../data/mock-books';
 
 @Injectable({
   providedIn: 'root'
 })
-export class BooksService {
 
-  constructor() { }
+export class BooksService {
+  constructor() {}
+
+  // Method to return an Observable of the BOOKS array
+  getBooks(): Observable<Book[]> {
+    return of(BOOKS); // Wrap the BOOKS array in an Observable
+  }
 }
