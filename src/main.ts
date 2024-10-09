0d4cfd17a8e40acf6d4499f42bcf8b3cdc2917cb
Kush Patel <w0859776@myscc.ca>
2024-10-09 13:28:28 -0400

    Defined routes of books-list and books-list-item

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { BooksListComponent } from './app/books-list/books-list.component';
+import { BooksListItemComponent } from './app/books-list-item/books-list-item.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  { path: 'books', component: BooksListItemComponent },
+  { path: 'books/:id', component: BooksListComponent}
+]
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+});
+
