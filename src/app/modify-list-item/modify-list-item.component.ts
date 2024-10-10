c1e46761bd5be6565fe3f3752e4676eff8aa0fc3
Kush Patel <w0859776@myscc.ca>
2024-10-09 23:47:05 -0400

    Imported router link and page not found component

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { PageNotFoundComponent } from "../page-not-found/page-not-found.component";
 
 @Component({
   selector: 'app-modify-list-item',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, PageNotFoundComponent],
   templateUrl: './modify-list-item.component.html',
   styleUrl: './modify-list-item.component.css'
 })
