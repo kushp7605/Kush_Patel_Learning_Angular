ffffeb2cfdbe1e94e4b9e2d13381903caf586e7b
Kush Patel <w0859776@myscc.ca>
2024-10-30 17:48:25 -0400

    Added methods for adding, updating books, and navigating back to book list

--- src/app/modify-list-item/modify-list-item.component.ts
+++ src/app/modify-list-item/modify-list-item.component.ts
@@ -1,14 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { PageNotFoundComponent } from "../page-not-found/page-not-found.component";
+import { BooksService } from '../services/books.service';
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import { Book } from '../Shared/Modules/book';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-modify-list-item',
   standalone: true,
-  imports: [RouterLink, PageNotFoundComponent],
+  imports: [RouterLink, NgIf, FormsModule, ReactiveFormsModule],
   templateUrl: './modify-list-item.component.html',
   styleUrl: './modify-list-item.component.css'
 })
-export class ModifyListItemComponent {
 
+export class ModifyListItemComponent implements OnInit {
+  bookForm: FormGroup;
+  book: Book | undefined; 
+
+  constructor (
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private booksService: BooksService,
+    private router: Router
+  ) {
+    this.bookForm = this.fb.group ({
+      id: ['', Validators.required], // ID is required
+      title: ['', Validators.required], // Title is required
+      author: ['', Validators.required], // Author is required
+      genre: ['', Validators.required], // Genre is required
+      publicationYear: [''],
+      rating: [''],
+    });
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.booksService.getBookById(+id).subscribe(book => {
+        if (book) {
+          this.book = book;
+
+          this.bookForm.patchValue(book);
+        }
+      });
+    }
+  }
+
+  onSubmit(): void {
+    const book: Book = this.bookForm.value;
+
+    // Check if we are updating an existing book
+    if(book.id) {
+      this.booksService.updateBook(book);
+    } else {
+      // For adding a new book, generate a new ID
+      const newId = this.booksService.generateNewId(); // This method will create a new ID
+      book.id = newId;
+      this.booksService.addBook(book);
+    }
+
+    this.router.navigate(['/books']);
+  }
+
+  // Navigate to the edit form with the selected book ID
+  editBook(bookId: number | undefined): void {
+    if (bookId !== undefined) {
+      this.router.navigate(['/modify-list-item', bookId]);
+    }
+  }
+
+  navigateToBookList(): void {
+    this.router.navigate(['/books']);
+  }
 }
