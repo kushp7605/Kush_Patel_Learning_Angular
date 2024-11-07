5efbb4a6ceb2e81db782e39b52321ac3d0365a14
Kush Patel <w0859776@myscc.ca>
2024-11-06 20:15:43 -0500

    Added error handling to fetch book errors and integrated BooksService in ngOnInit to fetch book list

--- src/app/books-list-item/books-list-item.component.ts
+++ src/app/books-list-item/books-list-item.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common'; 
 import { Book } from '../Shared/Modules/book';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink, Router} from '@angular/router';
+import { BooksService } from '../services/books.service';
 
 @Component({
   selector: 'app-books-list-item',
@@ -14,6 +15,35 @@ import { RouterLink } from '@angular/router';
 export class BooksListItemComponent {
   @Input() book?: Book; // Accepts a Book object
   @Input() showRating?: boolean; // Accepts showRating for visibility of ratings available for books or not
+
+  books: Book[] = [];
+  currentIndex: number = 0;
+  error: string | null = null;
+
+  constructor(private route: ActivatedRoute, private booksService: BooksService, private router: Router) {}
+
+  ngOnInit(): void {
+    // Fetch books using BooksService
+    this.booksService.getBooks().subscribe({
+      next: (books: Book[]) => {
+        this.books = books;
+        this.error = null; // Clear any previous errors
+
+        // Update view based on route paramMap
+        this.route.paramMap.subscribe(params => {
+          const id = Number(params.get('id'));
+          if (id) {
+            this.currentIndex = this.books.findIndex(book => book.id === id);
+            this.book = this.books[this.currentIndex];
+          }
+        });
+      },
+      error: (err) => {
+        this.error = 'Error fetching books';
+        console.error('Error fetching books:', err);
+      }
+    });
+  }
 
   // Method to toggle the showRating property
   toggleRating(): void {
