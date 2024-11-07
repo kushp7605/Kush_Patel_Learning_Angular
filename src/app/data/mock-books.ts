41a2fee06b430d469fb4b6ea37db4875595ba8c4
Kush Patel <w0859776@myscc.ca>
2024-11-06 20:23:17 -0500

    Removed image property from the books array

--- src/app/data/mock-books.ts
+++ src/app/data/mock-books.ts
@@ -2,8 +2,8 @@
 import { Book } from '../Shared/Modules/book';
 
 export const BOOKS: Book[] = [
-      { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: 1960, genre: 'Southern Gothic', image: "images/mockingbird.jpg"},
-      { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', publicationYear: 1999, genre: 'Fantasy', rating: 4.7, image: "images/harrypotter.jpg "},
-      { id: 3, title: 'Romeo and Juliet', author: 'William Shakespeare', publicationYear: 1597, genre: 'Tragedy', image: "images/romeojuliet.jpg" },
-      { id: 4, title: 'Marvel Comics', author: 'Stan Lee', publicationYear: 1939, genre: 'Superhero, Fantasy, Action, Adventure', rating: 5.0, image: "images/marvelcomic.jpg" },
+      { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: 1960, genre: 'Southern Gothic' },
+      { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', publicationYear: 1999, genre: 'Fantasy', rating: 4.7 },
+      { id: 3, title: 'Romeo and Juliet', author: 'William Shakespeare', publicationYear: 1597, genre: 'Tragedy' },
+      { id: 4, title: 'Marvel Comics', author: 'Stan Lee', publicationYear: 1939, genre: 'Superhero, Fantasy, Action, Adventure', rating: 5.0 },
     ];
