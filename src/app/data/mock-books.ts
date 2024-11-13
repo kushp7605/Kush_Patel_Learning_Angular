16c583ebea60ebd448017a6ebe788c3dbd8748d1
Kush Patel <w0859776@myscc.ca>
2024-11-12 21:18:56 -0500

    Added price and quote property in the books array

--- src/app/data/mock-books.ts
+++ src/app/data/mock-books.ts
@@ -2,8 +2,8 @@
 import { Book } from '../Shared/Modules/book';
 
 export const BOOKS: Book[] = [
-      { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: 1960, genre: 'Southern Gothic' },
-      { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', publicationYear: 1999, genre: 'Fantasy', rating: 4.7 },
-      { id: 3, title: 'Romeo and Juliet', author: 'William Shakespeare', publicationYear: 1597, genre: 'Tragedy' },
-      { id: 4, title: 'Marvel Comics', author: 'Stan Lee', publicationYear: 1939, genre: 'Superhero, Fantasy, Action, Adventure', rating: 5.0 },
+      { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee', publicationYear: '1960-07-11', genre: 'Southern Gothic', price: 25, quote: 'Best way to clear the air is to have it all out in the open!'},
+      { id: 2, title: 'Harry Potter and the Prisoner of Azkaban', author: 'J.K. Rowling', publicationYear: '1999-07-08', genre: 'Fantasy', price: 30, quote: 'Happiness can be found, even in the darkest of times, if one only remembers to turn on the light!', rating: 4.7 },
+      { id: 3, title: 'Romeo and Juliet', author: 'William Shakespeare', publicationYear: '1597-10-27', genre: 'Tragedy', price: 35, quote: 'Parting is such sweet sorrow that I shall say goodnight till it be morrow!'},
+      { id: 4, title: 'Marvel Comics', author: 'Stan Lee', publicationYear: '1939-08-31', genre: 'Superhero, Fantasy, Action, Adventure', price: 40, quote: 'I do not have inspiration. I only have ideas. Ideas and deadlines!' , rating: 5.0 },
     ];
