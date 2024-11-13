4e8f2486e17b147f29da6be5d742a9d51de2c19d
Kush Patel <w0859776@myscc.ca>
2024-11-12 21:26:07 -0500

    Added new properties price and quote for pipes

--- src/app/Shared/Modules/book.ts
+++ src/app/Shared/Modules/book.ts
@@ -2,8 +2,10 @@ export interface Book {
     id: number; // Required Property
     title: string; // Required Property
     author: string; // Required Property
-    publicationYear: number; // Required Property
+    publicationYear: string; // Required Property
     genre: string; // Required Property
+    price: number; // New Property
+    quote: string; // New Property
     rating?: number; // Optional Property
     showRating?: boolean; // Optional Property to track rating visibility
 }
