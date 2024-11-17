913ca0942134102665d48e55791721a7e0ee29b4
Kush Patel <w0859776@myscc.ca>
2024-11-17 00:50:56 -0500

    Created AutoFocus Directive to set focus on form input fields

---
+++ src/app/directives/auto-focus.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, AfterViewInit } from '@angular/core';
+
+@Directive({
+  selector: '[appAutoFocus]',
+  standalone: true,
+})
+export class AutoFocusDirective implements AfterViewInit {
+  constructor(private el: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.el.nativeElement.focus();
+  }
+}
