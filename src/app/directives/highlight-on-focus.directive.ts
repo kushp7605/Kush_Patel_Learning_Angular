3b8ae608dd4d7c02bdc3779b18d8f38657ffcdec
Kush Patel <w0859776@myscc.ca>
2024-11-17 01:01:57 -0500

    Implemented HighlightOnFocusDirective to dynamically highlight background color on focus and blur events

---
+++ src/app/directives/highlight-on-focus.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightOnFocus]',
+  standalone: true
+})
+export class HighlightOnFocusDirective {
+  @Input() appHighlightOnFocus = ''; // Highlight Color
+  @Input() appHighlightOnBlur = ''; // Default color after losing focus
+
+  constructor(private el: ElementRef) {}
+
+  @HostListener('focus') onFocus() {
+    this.highlight(this.appHighlightOnFocus || 'darkgoldenrod'); // Default focus color
+  }
+
+  @HostListener('blur') onBlur() {
+    this.highlight(this.appHighlightOnBlur || 'moccasin'); // Default blur color
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
