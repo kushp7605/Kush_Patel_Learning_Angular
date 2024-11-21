c06040be5dd6cdc7a4fc3416a885db57183f64e8
Kush Patel <w0859776@myscc.ca>
2024-11-21 15:59:48 -0500

    Implemented ShowDetailsOnHover Directive

---
+++ src/app/directives/show-details-on-hover.directive.spec.ts
@@ -0,0 +1,9 @@
+import { ShowDetailsOnHoverDirective } from './show-details-on-hover.directive';
+
+describe('ShowDetailsOnHoverDirective', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    const directive = new ShowDetailsOnHoverDirective();
+    expect(directive).toBeTruthy();
+  });
+});
