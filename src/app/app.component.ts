7a372415aeca572d5cd6cebd8fa99dccebed1bb2
Kush Patel <w0859776@myscc.ca>
2024-11-21 15:55:12 -0500

    Added date property and implemented method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common'; 
-import { Book } from './Shared/Modules/book';
-import { BooksListComponent } from "./books-list/books-list.component";
-import { BooksListItemComponent } from './books-list-item/books-list-item.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, BooksListComponent, BooksListItemComponent, RouterLink],
+  imports: [RouterOutlet, CommonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
+  date = Date.now();
   title = 'Books List App';
 }
