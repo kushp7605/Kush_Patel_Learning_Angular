c2446f2b7a58f766dbf752ab888fa55fd9f806f0
Kush Patel <w0859776@myscc.ca>
2024-11-28 23:04:28 -0500

    Imported MatButtonModule and MatToolbarModule material design components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, RouterLink],
+  imports: [RouterOutlet, CommonModule, RouterLink, MatButtonModule, MatToolbarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
