0aa12ad2b456d403963eb7f949268ba26e6c34de
Kush Patel <w0859776@myscc.ca>
2024-11-28 23:06:55 -0500

    Implemented routing with lazy loading

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter, Routes } from '@angular/router';
 import { BooksListComponent } from './app/books-list/books-list.component';
-import { BooksListItemComponent } from './app/books-list-item/books-list-item.component';
-import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
 import { ModifyListItemComponent } from './app/modify-list-item/modify-list-item.component';
 import { provideHttpClient } from "@angular/common/http";
 import { importProvidersFrom } from "@angular/core";
 import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 import { InMemoryDataService } from './app/services/in-memory-data.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 const routes: Routes = [
   { path: '', redirectTo: '/books', pathMatch: 'full' }, // Default route eagerly loaded
@@ -26,6 +25,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
-    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 }))
+    importProvidersFrom(HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 })), provideAnimationsAsync()
   ],
 }).catch((err) => console.error(err));
